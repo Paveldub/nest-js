6af6721733b81446bff016678fe8877dd0fb232c
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-07-20 19:41:46 +0300

    typeorm && postgre && ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { Module } from '@nestjs/common';
+// typeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './ormconfig';
+
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
