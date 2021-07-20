b46633830213348229260df22f7cb5295cb845d9
pavel.demidovich <pavel.demidovich@aventusgroup.com>
2021-07-20 16:33:28 +0300

    absolute paths

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
 
 @Controller('tags')
-
 export class TagController {
-  constructor(private readonly tagService: TagService){}
+  constructor(private readonly tagService: TagService) {}
+
   @Get()
   findAll(): string[] {
-    return this.tagService.findAll()
+    return this.tagService.findAll();
   }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-
 export class TagService {
   findAll(): string[] {
-    return ['weed', 'kush', 'dub']
+    return ['dub', 'weed', 'test'];
   }
 }
